34c09eb2525e441c83dff0f39ed7663279c930c4
fei0319 <fei0319@users.noreply.github.com>
2024-01-14 10:04:58 +0000

    Applied clang-format automatically (2024-01-14)

--- contest/cf1917/A.cpp
+++ contest/cf1917/A.cpp
@@ -3,28 +3,31 @@
 using ll = long long int;
 
 void solve(void) {
-	int n;
-	std::cin >> n;
-	std::vector<int> a(n);
-	int signedness = 1, p = 0;
-	for (int i = 0; i < n; ++i) {
-		std::cin >> a[i];
-		if (a[i] == 0) signedness = 0;
-		if (a[i] < 0) signedness *= -1;
-		if (a[i] > 0) p = i;
-	}
-	
-	switch (signedness) {
-		case 1:
-		std::cout << "1\n" << p + 1 << " 0\n";
-		break;
-		case -1:
-		std::cout << "0\n";
-		break;
-		case 0:
-		std::cout << "0\n";
-		break;
-	}
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    int signedness = 1, p = 0;
+    for (int i = 0; i < n; ++i) {
+        std::cin >> a[i];
+        if (a[i] == 0)
+            signedness = 0;
+        if (a[i] < 0)
+            signedness *= -1;
+        if (a[i] > 0)
+            p = i;
+    }
+
+    switch (signedness) {
+    case 1:
+        std::cout << "1\n" << p + 1 << " 0\n";
+        break;
+    case -1:
+        std::cout << "0\n";
+        break;
+    case 0:
+        std::cout << "0\n";
+        break;
+    }
 }
 
 int main(int argc, char *argv[]) {
