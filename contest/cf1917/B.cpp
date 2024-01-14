f82783ce20f869f6db50d30dd7a0755fc3a02e4f
fei0319 <fei0319@users.noreply.github.com>
2024-01-14 15:40:19 +0000

    Applied clang-format automatically (2024-01-14)

--- contest/cf1917/B.cpp
+++ contest/cf1917/B.cpp
@@ -3,19 +3,19 @@
 using ll = long long int;
 
 void solve(void) {
-	int n;
-	std::string s;
-	std::cin >> n >> s;
-	
-	std::unordered_set<char> st;
-	ll ans = 0;
-	for (int i = 0; i < n; ++i) {
-		ans += st.size();
-		st.insert(s[i]);
-	}
-	ans += st.size();
-	
-	std::cout << ans << '\n';
+    int n;
+    std::string s;
+    std::cin >> n >> s;
+
+    std::unordered_set<char> st;
+    ll ans = 0;
+    for (int i = 0; i < n; ++i) {
+        ans += st.size();
+        st.insert(s[i]);
+    }
+    ans += st.size();
+
+    std::cout << ans << '\n';
 }
 
 int main(int argc, char *argv[]) {
