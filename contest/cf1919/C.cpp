21739e7cc5848d5f358c429318e1f086b6137118
fei0319 <fei0319@users.noreply.github.com>
2024-01-14 07:55:08 +0000

    Applied clang-format and dos2unix

--- contest/cf1919/C.cpp
+++ contest/cf1919/C.cpp
@@ -3,24 +3,29 @@
 using ll = long long int;
 
 void solve(void) {
-	int n;
-	std::cin >> n;
-	std::vector<int> a(n);
-	for (int i = 0; i < n; ++i) std::cin >> a[i];
-	
-	int const INF = 1e9;
-	int x = INF, y = INF;
-	int ans = 0;
-	
-	for (int i : a) {
-		if (x > y) std::swap(x, y);
-		
-		if (i <= x) x = i;
-		else if (i <= y) y = i;
-		else x = i, ans += 1;
-	}
-	
-	std::cout << ans << '\n';
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n);
+    for (int i = 0; i < n; ++i)
+        std::cin >> a[i];
+
+    int const INF = 1e9;
+    int x = INF, y = INF;
+    int ans = 0;
+
+    for (int i : a) {
+        if (x > y)
+            std::swap(x, y);
+
+        if (i <= x)
+            x = i;
+        else if (i <= y)
+            y = i;
+        else
+            x = i, ans += 1;
+    }
+
+    std::cout << ans << '\n';
 }
 
 int main(int argc, char *argv[]) {
