b1c8e91a3afb773498e891ce3ec7c6734d842b5b
Fei Pan <pan-fei@outlook.com>
2024-03-06 17:55:18 +0800

    Added CF1715F(Unfinished)

---
+++ contest/cf1715/F.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using ll = long long int;
+using ld = long double;
+
+int n, m;
+
+ld get(int d) {
+    
+}
+
+int main() {
+    std::cin >> n >> m;
+    std::cout << std::fixed << std::setprecision(12);
+
+    ld x = get(0), y = get(1);
+    std::cout << "! " << x << ' ' << y << std::endl;
+
+    return 0;
+}
