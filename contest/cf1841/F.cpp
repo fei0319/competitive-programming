4892366a27241f14f4c64e7d69dbb7d09f268c86
fei0319 <fei0319@users.noreply.github.com>
2024-03-22 12:52:14 +0000

    Applied clang-format automatically (2024-03-22)

--- contest/cf1841/F.cpp
+++ contest/cf1841/F.cpp
@@ -55,7 +55,8 @@ int main() {
     });
     t = 0;
     for (int i = 1; i <= n; ++i) {
-        if (t && p[i] * p[t] == 0 && (ld)p[i].x * p[t].x + (ld)p[i].y * p[t].y > 0) {
+        if (t && p[i] * p[t] == 0 &&
+            (ld)p[i].x * p[t].x + (ld)p[i].y * p[t].y > 0) {
             p[t] = p[t] + p[i];
         } else {
             p[++t] = p[i];
@@ -67,13 +68,15 @@ int main() {
 
     ld ans = 0;
     int l = 1;
-    Point sum {0, 0};
+    Point sum{0, 0};
     for (int i = 1; i <= n + n; ++i) {
-        while (i - l + 1 > n || p[i] * p[l] > 0 || (p[i] * p[l] == 0 && (ll)p[i].x * p[l].x + (ll)p[i].y * p[l].y < 0)) {
+        while (i - l + 1 > n || p[i] * p[l] > 0 ||
+               (p[i] * p[l] == 0 &&
+                (ll)p[i].x * p[l].x + (ll)p[i].y * p[l].y < 0)) {
             sum = sum - p[l++];
             ans = std::max(ans, (ld)sum.x * sum.x + (ld)sum.y * sum.y);
         }
-        
+
         sum = sum + p[i];
         ans = std::max(ans, (ld)sum.x * sum.x + (ld)sum.y * sum.y);
     }
