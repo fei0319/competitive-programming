75917c76a20610e8fa9bb2c16dcc98030f38d9a9
fei0319 <fei0319@users.noreply.github.com>
2024-04-13 09:10:50 +0000

    Applied clang-format automatically (2024-04-13)

--- contest/cf1773/G.cpp
+++ contest/cf1773/G.cpp
@@ -7,58 +7,58 @@ constexpr int MAXN = 2e5 + 19;
 
 template <typename It>
 void fwt(It f, int l, int r) {
-	if (l == r) {
-		return;
-	}
-	int mid = (l + r) / 2;
-	fwt(f, l, mid);
-	fwt(f, mid + 1, r);
-	for (int i = mid + 1; i <= r; ++i) {
-		f[i] += f[i - (mid + 1) + l];
-	}
+    if (l == r) {
+        return;
+    }
+    int mid = (l + r) / 2;
+    fwt(f, l, mid);
+    fwt(f, mid + 1, r);
+    for (int i = mid + 1; i <= r; ++i) {
+        f[i] += f[i - (mid + 1) + l];
+    }
 }
 
 template <typename It>
 void ifwt(It f, int l, int r) {
-	if (l == r) {
-		return;
-	}
-	int mid = (l + r) / 2;
-	for (int i = mid + 1; i <= r; ++i) {
-		f[i] -= f[i - (mid + 1) + l];
-	}
-	ifwt(f, l, mid);
-	ifwt(f, mid + 1, r);
+    if (l == r) {
+        return;
+    }
+    int mid = (l + r) / 2;
+    for (int i = mid + 1; i <= r; ++i) {
+        f[i] -= f[i - (mid + 1) + l];
+    }
+    ifwt(f, l, mid);
+    ifwt(f, mid + 1, r);
 }
 
 int n, m;
 int a[1 << 20], cnt[1 << 20];
 void solve(std::vector<ld> &dp, int l, int r) {
-	static ld f[MAXN];
-	
-	if (l == r) {
-		return;
-	}
-	int mid = (l + r) / 2;
-	solve(dp, l, mid);
-	
-	for (int i = l; i <= mid; ++i) {
-		f[i] = dp[i] / (n - cnt[i]);
-	}
-	for (int i = mid + 1; i <= r; ++i) {
-		f[i] = 0;
-	}
-	
-	fwt(f, l, r);
-	for (int i = l; i <= r; ++i) {
-		f[i] *= a[i];
-	}
-	ifwt(f, l, r);
-	for (int i = mid + 1; i <= r; ++i) {
-		dp[i] += f[i];
-	}
-	
-	solve(dp, mid + 1, r);
+    static ld f[MAXN];
+
+    if (l == r) {
+        return;
+    }
+    int mid = (l + r) / 2;
+    solve(dp, l, mid);
+
+    for (int i = l; i <= mid; ++i) {
+        f[i] = dp[i] / (n - cnt[i]);
+    }
+    for (int i = mid + 1; i <= r; ++i) {
+        f[i] = 0;
+    }
+
+    fwt(f, l, r);
+    for (int i = l; i <= r; ++i) {
+        f[i] *= a[i];
+    }
+    ifwt(f, l, r);
+    for (int i = mid + 1; i <= r; ++i) {
+        dp[i] += f[i];
+    }
+
+    solve(dp, mid + 1, r);
 }
 
 int main() {
@@ -69,9 +69,9 @@ int main() {
 
     m -= 1;
     int T = 1 << m;
-    
+
     int g = 0;
-    
+
     for (int i = 1; i <= n; ++i) {
         std::string s;
         std::cin >> s;
@@ -84,31 +84,31 @@ int main() {
         if (s[0] == '1') {
             v ^= (1 << m) - 1;
             a[v] += 1;
-            
+
             g |= v;
         }
-        
+
         cnt[v] += 1;
     }
 
     for (int s = T - 1; s >= 1; --s) {
-    	if (cnt[s] && (s & g) != s) {
-    		std::cout << "0\n";
-    		return 0;
-    	}
-    	for (int i = (s - 1) & s; ; i = (i - 1) & s) {
-    		cnt[s] += cnt[i];
-    		if (i == 0) {
-    			break;
-    		}
-    	}
+        if (cnt[s] && (s & g) != s) {
+            std::cout << "0\n";
+            return 0;
+        }
+        for (int i = (s - 1) & s;; i = (i - 1) & s) {
+            cnt[s] += cnt[i];
+            if (i == 0) {
+                break;
+            }
+        }
     }
-    
+
     std::vector<ld> dp(T);
     dp[0] = 1;
     fwt(a, 0, T - 1);
     solve(dp, 0, T - 1);
-    
+
     std::cout << std::fixed << std::setprecision(16);
     std::cout << dp[g] << '\n';
 
