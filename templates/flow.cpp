159422ac4be23c85b32383ace804a23e1bbb24c9
Fei Pan <pan-fei@outlook.com>
2024-05-08 19:14:44 +0800

    Flow::add_edge now returns edge id

--- templates/flow.cpp
+++ templates/flow.cpp
@@ -1,5 +1,3 @@
-#include <bits/stdc++.h>
-
 template <class T>
 struct Flow {
     static constexpr int INF = 1e9;
@@ -9,7 +7,7 @@ struct Flow {
         T cap;
         Edge(int to, T cap) : to(to), cap(cap) {}
     };
-    std::vector<Edge> e;
+    std::vector<Edge> edge;
     std::vector<std::vector<int>> adj;
     std::vector<int> cur, dep;
     Flow(int n) : n(n), adj(n) {}
@@ -22,8 +20,8 @@ struct Flow {
             int node = q.front();
             q.pop();
             for (int i : adj[node]) {
-                int to = e[i].to;
-                T c = e[i].cap;
+                int to = edge[i].to;
+                T c = edge[i].cap;
                 if (c > 0 && dep[to] == -1) {
                     dep[to] = dep[node] + 1;
                     if (to == t) {
@@ -42,15 +40,15 @@ struct Flow {
         T f, res = 0;
         for (int &i = cur[node]; i < int(adj[node].size()); ++i) {
             int j = adj[node][i];
-            int to = e[j].to;
-            T c = e[j].cap;
+            int to = edge[j].to;
+            T c = edge[j].cap;
 
             if (dep[to] == dep[node] + 1 &&
                 (f = dfs(to, t, std::min(flow, c)))) {
                 res += f;
                 flow -= f;
-                e[j].cap -= f;
-                e[j ^ 1].cap += f;
+                edge[j].cap -= f;
+                edge[j ^ 1].cap += f;
             }
 
             if (flow == 0) {
@@ -62,11 +60,13 @@ struct Flow {
         }
         return res;
     }
-    void add_edge(int u, int v, T c) {
-        adj[u].push_back(e.size());
-        e.emplace_back(v, c);
-        adj[v].push_back(e.size());
-        e.emplace_back(u, 0);
+    int add_edge(int u, int v, T c) {
+        int j = edge.size();
+        adj[u].push_back(edge.size());
+        edge.emplace_back(v, c);
+        adj[v].push_back(edge.size());
+        edge.emplace_back(u, 0);
+        return j;
     }
     T max_flow(int s, int t) {
         T ans = 0;
